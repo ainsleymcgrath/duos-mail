c2626db16be10a1ca92b3c6895c2bcf07d310428
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2018-12-12 21:07:12 -0600

    multiple recipients, no substitution
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import sendgrid
 import os
-from sendgrid.helpers.mail import *
+
+
+# from sqlalchemy.engine.url import URL
+# from sqlalchemy import create_engine, MetaData
+
+SOME_RECIPIENTS = [
+    {
+        "name": "Arbus",
+        "paper_title": "Flarbus",
+        "email": "ainsley.mcgrath@aptitive.com",
+    },
+    {"name": "Blumbos", "paper_title": "bambu!", "email": "mcgrath.ainsley@gmail.com"},
+]
+
+
+def compose_sendgrid_post_body(recipients_with_details=[], subject=""):
+    return {
+        "personalizations": [
+            {
+                "to": [{"email": recipient["email"]}],
+                "subject": subject,
+                "substitutions": {
+                    "-author_name-": recipient["name"],
+                    "-paper_title-": recipient["paper_title"],
+                },
+            }
+            for recipient in recipients_with_details
+        ],
+        "from": {"email": "test@example.com"},
+        "content": [
+            {
+                "type": "text/plain",
+                "value": "Hello, -author_name-,\nWhat's up with your pape called -paper_title-??",
+            }
+        ],
+    }
+
 
 sg = sendgrid.SendGridAPIClient(apikey=os.environ.get("SENDGRID_API_KEY"))
-from_email = Email("test@example.com")
-to_email = Email("mcgrath.ainsley@gmail.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
 
-response = sg.client.mail.send.post(request_body=mail.get())
+print(compose_sendgrid_post_body(SOME_RECIPIENTS))
+
+response = sg.client.mail.send.post(
+    request_body=compose_sendgrid_post_body(
+        recipients_with_details=SOME_RECIPIENTS, subject="awesome test"
+    )
+)
 print(response.status_code)
-print(response.body)
-print(response.headers)
